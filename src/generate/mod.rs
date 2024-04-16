39e4a1004d99b790fbf87feeac8a20dc253ad011
clement <clementwanjau@gmail.com>
2024-04-16 05:48:50 +0300

    Add a function to generate ed25519 pub and private keys.

--- src/generate/mod.rs
+++ src/generate/mod.rs
@@ -1,3 +1,5 @@
+use ed25519_dalek::pkcs8::{EncodePrivateKey, EncodePublicKey};
+use rand::rngs::OsRng;
 use crate::{error::Error, Result};
 use rand::thread_rng;
 use rsa::{
@@ -5,6 +7,7 @@ use rsa::{
     RsaPrivateKey, RsaPublicKey,
 };
 
+#[derive(Debug)]
 pub struct DkimKeyPair {
     pub private_key_x509: String,
     pub public_key_x509: String,
@@ -29,5 +32,51 @@ impl DkimKeyPair {
             private_key_x509: String::from_utf8_lossy(&private_key_x509).to_string(),
             public_key_x509: String::from_utf8_lossy(&public_key_x509).to_string(),
         })
+    }
+
+    pub fn generate_ed25519() -> Result<Self> {
+        let mut csprng = OsRng;
+        // Generate the ED25519 key pair
+        let signing_key = ed25519_dalek::SigningKey::generate(&mut csprng);
+        let verifying_key = signing_key.verifying_key();
+        // Encode the keys in X509 format
+        let priv_key_x509 = signing_key
+            .to_pkcs8_pem(Default::default())
+            .map_err(Error::Ed25519)?
+            .as_bytes()
+            .to_owned();
+        let pub_key_x509 = verifying_key
+            .to_public_key_pem(Default::default())
+            .map_err(|e| Error::Generic(e.to_string()))?
+            .as_bytes()
+            .to_owned();
+        
+        Ok(DkimKeyPair {
+            private_key_x509: String::from_utf8_lossy(&priv_key_x509).to_string(),
+            public_key_x509: String::from_utf8_lossy(&pub_key_x509).to_string(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_rsa() {
+        let key_pair = DkimKeyPair::generate_rsa(2048).unwrap();
+        assert!(key_pair.private_key_x509.starts_with("-----BEGIN RSA PRIVATE KEY-----"));
+        assert!(key_pair.public_key_x509.starts_with("-----BEGIN RSA PUBLIC KEY-----"));
+        println!("{}", key_pair.private_key_x509);
+        println!("{}", key_pair.public_key_x509);
+    }
+
+    #[test]
+    fn test_generate_ed25519() {
+        let key_pair = DkimKeyPair::generate_ed25519().unwrap();
+        assert!(key_pair.private_key_x509.starts_with("-----BEGIN PRIVATE KEY-----"));
+        assert!(key_pair.public_key_x509.starts_with("-----BEGIN PUBLIC KEY-----"));
+        println!("{}", key_pair.private_key_x509);
+        println!("{}", key_pair.public_key_x509);
     }
 }
